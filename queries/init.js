ceefff1f98e55deab5fd6babcf183a6ec57365d0
Pavel Sapezhko <psapezhko@rocketdata.io>
2020-05-27 15:30:36 +0300

    Adopt to latest mongo version

--- queries/init.js
+++ queries/init.js
@@ -10,12 +10,12 @@ sh.disableBalancing("test.sample");
 
 db.sample.drop();
 
-db.createCollection("sample"); 
+db.createCollection("sample");
 
-db.sample.createIndex( { factoryId: 1 } );
-
+db.sample.createIndex( { factoryId: 1 } );
+
 sh.enableSharding("test");
-
+
 sh.shardCollection("test.sample",{ location: 1, factoryId: 1});
 
 sh.addTagRange(
@@ -35,19 +35,19 @@ sh.addTagRange(
 
 
 
-sh.enableBalancing("test.sample");
-
-for(var i=0; i<100; i++){
-    db.sample.insert({
-        "location": "US",
-        "factoryId": NumberInt(i)
-    });
-    
-    db.sample.insert({
-        "location": "EU",
-        "factoryId": NumberInt(100+i)
-    });
-}
+sh.enableBalancing("test.sample");
+
+for(var i=0; i<100; i++){
+    db.sample.insert({
+        "location": "US",
+        "factoryId": NumberInt(i)
+    });
+
+    db.sample.insert({
+        "location": "EU",
+        "factoryId": NumberInt(100+i)
+    });
+}
 
 sh.startBalancer();
 
